a58d823601bab2f4369e9a5855e653faf0443c53
Gavin Coleman <gavinjcoleman@gmail.com>
2023-01-19 21:41:04 +0000

    ok to 9:00 on vid, check </form> position

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -22,6 +22,23 @@ function CreateListing() {
 		longitude: 0,
 	});
 
+	// De-structuring
+	const {
+		type,
+		name,
+		bedrooms,
+		bathrooms,
+		parking,
+		furnished,
+		address,
+		offer,
+		regularPrice,
+		discountedPrice,
+		images,
+		latitude,
+		longitude,
+	} = formData
+
 	const auth = getAuth();
 	const navigate = useNavigate();
 	const isMounted = useRef(true);
@@ -44,11 +61,182 @@ function CreateListing() {
 		//eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isMounted]);
 
+	const onSubmit = (e) => {
+		e.preventDefault()
+	}
+
+	const onMutate = (e) => {}
+
 	if (loading) {
 		return <Spinner />;
 	}
 
-	return <div>Create</div>;
+	return <div className='profile'>
+		<header>
+			<p className="pageheader">Create a Listing</p>
+		</header>
+
+		<main>
+			<form onSubmit={onSubmit}>
+				<label className='formLabel'>Sell / Rent</label>
+				<div className="formButtons">
+					<button 
+						type ='button'
+						className={type === 'sale'? 'formButtonActive' :
+						'formButton'}
+						id='type'
+						value='sale'
+						onClick={onMutate}
+					>
+						Sell
+					</button>
+					<button 
+						type ='button'
+						className={type === 'rent'? 'formButtonActive' :
+						'formButton'}
+						id='type'
+						value='rent'
+						onClick={onMutate}
+					>
+						Rent
+					</button>
+				</div>
+
+				<label className="formLabel">Name</label>
+				<input 
+					type="text" 
+					className="formInputName"
+					id='name'
+					value={name}
+					onChange={onMutate}
+					maxLength='32'
+					minLength='10'
+					required
+				/>
+
+			<div className="formRooms flex">
+				<div>
+					<label className='formLabel'>Bedrooms</label>
+					<input 
+						className='formInputSmall'
+						type="number"
+						id='bedrooms'
+						value={bedrooms}
+						onChange={onMutate}
+						min='1'
+						max='50'
+						required					
+					/>
+				</div>
+				<div>
+					<label className='formLabel'>Bathrooms</label>
+					<input 
+						className='formInputSmall'
+						type="number"
+						id='bathrooms'
+						value={bathrooms}
+						onChange={onMutate}
+						min='1'
+						max='50'
+						required					
+					/>
+				</div>
+			</div>
+
+			<label className="formLabel">Parking Spot</label>
+			<div className="formButtons">
+				<button
+					className={parking ? 'formButtonActive' :
+					'formButton'}
+					type='button'
+					id='parking'
+					value={true}
+					onClick={onMutate}
+					min='1'
+					max='50'
+				>
+					Yes
+				</button>
+				<button
+					className={
+						!parking && parking !== null ? 'formButtonActive'
+						: 'formButton'
+					}
+					type='button'
+					id='parking'
+					value={false}
+					onlick={onMutate}
+				>
+					No
+				</button>
+			</div>
+
+			<label className="formLabel">Furnished</label>
+			<div className="formButtons">
+				<button
+					className={furnished ? 'formButtonActive' :
+					'formButton'}
+					type='button'
+					id='furnished'
+					value={true}
+					onClick={onMutate}
+				>
+					Yes
+				</button>
+				<button
+					className={
+						!furnished && furnished !== null
+							? 'formButtonActive'
+							: 'formButton'
+					}
+					type='button'
+					id='furnished'
+					value={false}
+					onlick={onMutate}
+				>
+					No
+				</button>
+			</div>
+
+			<label className="formLabel">Address</label>
+			<textarea
+				className='formInputAddress'
+			  type='text'
+				id="address"
+				value={address}
+				onChange={onMutate}
+				required				
+			/>
+
+			{!geolocationEnabled && (
+				<div className="formLatLng flex">
+					<div>
+						<label className="formLabel">Latitude</label>
+						<input
+							className='formInputSmall'
+							type="number"
+							id='latitude'
+							value={latitude}
+							onChange={onMutate}
+							required	
+						/>
+					</div>
+					<div>
+						<label className="formLabel">Longitude</label>
+						<input
+							className='formInputSmall'
+							type="number"
+							id='longitude'
+							value={longitude}
+							onChange={onMutate}
+							required	
+						/>
+					</div>
+				</div>
+			)}
+			</form>		
+		</main>
+	</div>;
 }
 
 export default CreateListing;
